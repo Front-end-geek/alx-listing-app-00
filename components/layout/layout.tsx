1022199a72898bc92ffa7a826e381498f880137f
unknown <IamHendy>
2024-12-22 17:17:36 +0300

    tasks

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 const Layout: React.FC <{children: React.ReactNode }> = ({children}) => (
     <>
     <Header/>
-    <main className="min-h-screen">{children}</main>
+    <main className="min-h-screen text-black">{children}</main>
     <Footer/>
     </>
 )
